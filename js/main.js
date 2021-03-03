d2e74cd4774a9c33f19d3eb7a810c5c2a7b66cce
Adrian Chaves Ghilardi <adrian.chaves@gmail.com>
2021-03-03 11:18:15 -0300

    objetos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,37 @@
+class Equipo {
+    constructor(nombre) {
+        this.validarNombre(nombre);
+    }
+    validarNombre(nombre) {
+        let nombreTrim = nuevoNombre.trim();
+         if (nombreTrim.length > 2){
+             this.nombre = nombreTrim;
+
+         }
+    }
+
+}
+
+class Turno {
+
+    constructor(dia, horaInicio, horaFin) {
+        this.dia = dia;
+        this.horaInicio = horaInicio;
+        this.horaFin = horaFin;
+    }
+}
+class Cancha {
+    constructor(id) {
+        this.id = id;
+    }
+
+}
 //ingresar equipo tiene que haber mas de un equipo
 entrada = ingresarEquipo();
-if(entrada.length < 1) {
+if (entrada.length < 1) {
     alert("Debe ingresar al menos dos equipos");
-}else {
-    alert("ingresó " + entrada + " equipos" )
+} else {
+    alert("ingresó " + entrada + " equipos")
 }
 //elegir dia y horario
 //mostrar canchas disponibles 
@@ -13,10 +41,23 @@ function ingresarEquipo() {
     let entrada = prompt("Ingresar Nombre de los Equipos o q para continuar");
     let equipos = [];
 
-    while(entrada != "q") { 
-        equipos.push(entrada);
-        
+    while (entrada != "q") {
+        if (entrada.length != 0) {
+            let equipo = new Equipo(entrada);
+            equipos.push(equipo);
+
+        }
+
         entrada = prompt("Ingresar Nombre de los Equipos o q para continuar");
     }
     return equipos;
+}
+
+//crear canchas
+function crearCancha(){
+    turnos = [];
+    turnos.push(new Turno(dia,17,18));
+    turnos.push(new Turno(dia,18,19));
+    
+    const cancha1 = new Cancha(1, turnos);
 }
